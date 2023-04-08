eb23828ec14d8762038e0a9ccbce2982e25d1e2c
aykutcayir34 <aykut.cayir@khas.edu.tr>
2023-04-08 22:40:52 +0300

    harmonic oscillator has been added.

---
+++ harmonic_oscillator.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from neurodiffeq import diff
+from neurodiffeq.ode import solve
+from neurodiffeq.conditions import IVP
+
+dduddt = lambda u, t: diff(u, t, order=2) + u
+u = lambda t: np.sin(t)
+
+init_val_ho = IVP(t_0=0.0, u_0=0, u_0_prime=1.0)
+solution_ho, _ = solve(ode=dduddt, condition=init_val_ho, t_min=0.0, t_max=2*np.pi, max_epochs=3000)
+ts = np.linspace(0.0, 2*np.pi, 50)
+u_net = solution_ho(ts, to_numpy=True)
+u_ana = u(ts)
+
+plt.figure()
+plt.plot(ts, u_net, label="NN Based Solution")
+plt.plot(ts, u_ana, ".", label="Analytical Solution")
+plt.ylabel("u(t)")
+plt.xlabel("t")
+plt.title("Harmonic Oscillator")
+plt.legend()
+plt.show()
+plt.savefig("harmonic_oscillator.png")
